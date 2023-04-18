a044d9cfa77b70e24499be338ab15854824d5edc
Pichborith Kong <pichborith@gmail.com>
2023-04-18 12:29:53 -0700

    add new post button to home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,25 @@
+import { Link } from 'react-router-dom';
 import { HomeProps } from '../types/types';
 
 const Home = ({ posts }: HomeProps) => {
   return (
-    <div className='posts'>
-      {posts.map((post) => (
-        <div className='post' key={post._id}>
-          <h2>{post.title}</h2>
-          <p>{post.__v} view(s)</p>
-          <span>{post.description}</span>
-          <p>{post.price}</p>
-          <p>{post.author.username}</p>
-          <p>{post.location}</p>
-        </div>
-      ))}
+    <div className='home'>
+      <form>
+        <input placeholder='Search' />
+        <Link to='/new'>New Post</Link>
+      </form>
+      <div className='posts'>
+        {posts.map((post) => (
+          <div className='post' key={post._id}>
+            <h2>{post.title}</h2>
+            <p>{post.__v} view(s)</p>
+            <span>{post.description}</span>
+            <p>{post.price}</p>
+            <p>{post.author.username}</p>
+            <p>{post.location}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
