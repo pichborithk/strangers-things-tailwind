896115cba5fe71b9a0e54aac318e6bcf8f8c2988
Pichborith Kong <pichborith@gmail.com>
2023-04-24 20:14:09 -0700

    update style on navbar and signin form

--- src/router/SignIn.tsx
+++ src/router/SignIn.tsx
@@ -36,10 +36,13 @@ const SignIn = () => {
   }, []);
 
   return (
-    <div className='signin'>
-      <form onSubmit={handleSignIn}>
+    <div className='signin mx-auto flex h-screen max-w-6xl items-center justify-center'>
+      <form
+        onSubmit={handleSignIn}
+        className='flex w-1/2 flex-col items-center justify-evenly gap-12 rounded-2xl border border-solid border-red-100 px-20 py-12 text-xl text-red-500 shadow-md'
+      >
         <h1>Sign In</h1>
-        <fieldset>
+        <fieldset className='flex w-full flex-col'>
           <label htmlFor='username'>Username</label>
           <input
             type='text'
@@ -48,9 +51,10 @@ const SignIn = () => {
             value={username}
             onChange={event => setUsername(event.target.value)}
             required
+            className='rounded-md border border-solid border-slate-300 px-4 py-2 focus:outline-red-500'
           />
         </fieldset>
-        <fieldset>
+        <fieldset className='flex w-full flex-col'>
           <label htmlFor='password'>Password</label>
           <input
             type={hidePassword ? 'password' : 'text'}
@@ -59,6 +63,7 @@ const SignIn = () => {
             value={password}
             onChange={event => setPassword(event.target.value)}
             required
+            className='rounded-md border border-solid border-slate-300 px-4 py-2 focus:outline-red-500'
           />
           <i
             className={`fa-solid  ${hidePassword ? 'fa-eye-slash' : 'fa-eye'} ${
@@ -67,7 +72,7 @@ const SignIn = () => {
             onClick={() => setHidePassword(!hidePassword)}
           ></i>
         </fieldset>
-        <div>
+        <div className='w-3/4 text-center'>
           <button>Sign In</button>
           <p>
             Forget <a>Username / Password</a>?
